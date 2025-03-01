92b9366626a64a9246b70a2c195970280a955bf8
DarkMatters0 <hz202300480@wmsu.edu.ph>
2025-03-01 08:29:51 +0800

    Added Offices, includes for referencing navbar for less code

---
+++ includes/navbar.php
@@ -0,0 +1,33 @@
+ <!-- Header Section - Full width -->
+ <div class="news-bar">
+        <div class="news-left">
+            <span>News:</span>
+            <span>WMSU ILS Elementary</span>
+            <div class="icon-circle">✓</div>
+            <div class="icon-circle">❤</div>
+        </div>
+        <div class="contact-info">
+            <a href="tel:991-1771">
+                <img src="path/to/phone-icon.png" alt="Phone">
+                991-1771
+            </a>
+            <a href="mailto:wmsu@wmsu.edu.ph">
+                <img src="path/to/email-icon.png" alt="Email">
+                wmsu@wmsu.edu.ph
+            </a>
+            <span>ISO 9001:2015</span>
+        </div>
+    </div>
+    
+    <!-- Navigation Bar - Full width -->
+    <div class="nav-bar">
+        <a href="#">Home</a>
+        <a href="#">About Us</a>
+        <a href="#">Admission</a>
+        <a href="#">Administration</a>
+        <a href="#">Procurement</a>
+        <a href="#">Research</a>
+        <a href="#">Career</a>
+        <a href="#">Other Links</a>
+        <button class="mywmsu-button">MyWMSU</button>
+    </div>
