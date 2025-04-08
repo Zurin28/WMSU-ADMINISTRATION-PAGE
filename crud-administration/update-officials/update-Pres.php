122c6ba71289309e2a15fe15e37167d8e5275632
DarkMatters0 <hz202300480@wmsu.edu.ph>
2025-04-08 13:16:50 +0800

    working pres update in administrative official copy

--- crud-administration/update-officials/update-Pres.php
+++ crud-administration/update-officials/update-Pres.php
@@ -1,27 +1,41 @@
+
 <?php
+
 require_once('../../tools/functions.php');
 require_once('../../classes/Pres.class.php');
 
 $id = $_GET['id'];
-$name = $title = $page_link = '';
+$name = $title = $title_bor = $page_link = '';
 
 $pres = new Pres();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $name = clean_input($_POST['name']);
     $title = clean_input($_POST['title']);
+    $title_bor = clean_input($_POST['title']);
     $page_link = clean_input($_POST['page_link']);
 
-    // Set the properties of the Pres object
-    $pres->id = $id;
-    $pres->name = $name;
-    $pres->title = $title;
-    $pres->page_link = $page_link;
+    $file_name = null; // Default to null if no image is provided
 
-    if ($pres->edit()) {
+    // Check if an image is uploaded
+    if (!empty($_FILES['image']['name'])) {
+        $file_name = $_FILES['image']['name'];
+        $tempname = $_FILES['image']['tmp_name'];
+        $folder = '../../images/' . $file_name;
+
+        // Move uploaded file to the 'images' folder
+        if (!move_uploaded_file($tempname, $folder)) {
+            echo json_encode(['status' => 'error', 'message' => 'Failed to upload the image.']);
+            exit;
+        }
+    }
+
+    // Perform the update (with or without image)
+    if ($pres->edit($id, $name, $title, $title_bor, $page_link, $file_name)) {
         echo json_encode(['status' => 'success']);
     } else {
         echo json_encode(['status' => 'error', 'message' => 'Something went wrong when updating the record.']);
     }
     exit;
 }
+?>
