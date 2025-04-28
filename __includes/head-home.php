68513abfb9de5befb36e52728c0515a6e7e5bb90
DarkMatters0 <hz202300480@wmsu.edu.ph>
2025-04-29 01:16:50 +0800

    homepage w backend

--- __includes/head-home.php
+++ __includes/head-home.php
@@ -6,25 +6,12 @@
     <title></title>
 
     <style>
-        <?php require_once '../vendor/bootstrap-5.3.3-dist/css/bootstrap.min.css'; ?>
-        <?php require_once '../vendor/datatable-2.1.8/datatables.min.css'; ?>
+
         <?php require_once '../css/navbar.css'; ?>
 
+        <?php require_once '../css/home.css'; ?>
+        <?php require_once '../css/new.css'; ?>
 
     </style>
 
-    <?php # Lateef Fonts ?>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lateef:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
-
-    <?php # Montserrat Fonts ?>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-
-    <?php #Inter Fonts ?>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
 </head>
